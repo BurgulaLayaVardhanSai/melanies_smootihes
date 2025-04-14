f97c4671c3fde3ea6d2f08b063bad09279f3fcfb
BurgulaLayaVardhanSai <158276915+BurgulaLayaVardhanSai@users.noreply.github.com>
2025-04-14 14:57:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,3 +39,7 @@ if ingredient_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered {name_on_order}!', icon="✅")
+
+  smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/all")
+  sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
+
